186ab373cee158fc0468474ded9cee646e2e2e2f
Karl Higley <kmhigley@gmail.com>
2020-06-19 11:07:42 -0400

    Add a hypothesis strategy for generating scores and labels

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 import pytest
 import torch
 
+import hypothesis
+from hypothesis import strategies as strat
+from hypothesis import assume, given
+
 
 @pytest.fixture(scope="module")
 def num_items() -> int:
@@ -26,3 +30,23 @@ def labels(num_items: int) -> torch.Tensor:
 def scores(num_items: int) -> torch.Tensor:
     """A large tensor of simulated relevance scores"""
     return torch.empty(num_items).uniform_(0, 1)
+
+
+@strat.composite
+def scores_at_ks(draw, label_strat=strat.floats):
+    """Test case generation strategy for cutoffs and pairs of true/pred score lists"""
+    length = draw(strat.integers(min_value=2, max_value=10))
+    cutoffs_list = strat.lists(
+        strat.integers(min_value=length, max_value=length), min_size=1, max_size=10
+    )
+    scores_list = strat.lists(
+        strat.floats(min_value=0, max_value=1),
+        min_size=length,
+        max_size=length,
+        unique=True,
+    )
+    labels_list = strat.lists(
+        label_strat(min_value=0, max_value=1), min_size=length, max_size=length,
+    )
+
+    return (draw(cutoffs_list), draw(scores_list), draw(labels_list))
