56b00db74453a34878a3cb43e437c3c1af7b8dc9
Karl Higley <kmhigley@gmail.com>
2020-07-07 13:54:54 -0400

    Speed up `_create_output_placeholder` by creating directly on device
    Creating the placeholder on the CPU and then sending it to the GPU is much slower than creating it directly on the GPU.

--- ranking_metrics_torch/common.py
+++ ranking_metrics_torch/common.py
@@ -35,6 +35,6 @@ def _extract_topk(ks, scores, labels):
 
 
 def _create_output_placeholder(scores, ks):
-    return torch.zeros(scores.shape[0], len(ks)).to(
-        device=scores.device, dtype=torch.float64
+    return torch.zeros(
+        scores.shape[0], len(ks), dtype=torch.float64, device=scores.device
     )
